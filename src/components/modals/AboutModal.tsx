23e8e283d71e827a459e5126be2d307d5d5ec2ed
wongbener <85409699+wongbener@users.noreply.github.com>
2022-01-12 20:10:08 +0700

    Update AboutModal.tsx

--- src/components/modals/AboutModal.tsx
+++ src/components/modals/AboutModal.tsx
@@ -56,17 +56,17 @@ export const AboutModal = ({ isOpen, handleClose }: Props) => {
                     <p className="text-sm text-gray-500">
                       This is an open source clone of the game Wordle -{" "}
                       <a
-                        href="https://github.com/hannahcode/wordle"
+                        href="https://kevin.games"
                         className="underline font-bold"
                       >
-                        check out the code here
+                        Wordle Game
                       </a>{" "}
                       and{" "}
                       <a
-                        href="https://www.powerlanguage.co.uk/wordle/"
+                        href="https://kevin.games"
                         className="underline font-bold"
                       >
-                        play the original here
+                        Check Other Games
                       </a>
                     </p>
                   </div>
